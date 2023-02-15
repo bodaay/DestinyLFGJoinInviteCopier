a4594f73b4b5151dfc9b2cd35ad3df7bdda8d74d
khalefa <khalefa@alahmad.org>
2023-02-15 04:29:22 +0300

    fixed regex

--- DestinyLFGJoinInviteCopier.plugin.js
+++ DestinyLFGJoinInviteCopier.plugin.js
@@ -60,7 +60,7 @@ module.exports = (() => {
   
   // const regex = /(?<!\w)\/?[ur]\/[\w\d-]{2,21}/g
   // const regex = /(?<!\w)\/join ([A-Za-z0-9_ @!:()$%&*~.])*#\d{4}/gi
-  const regex = new RegExp("\\/(join|invite) ([A-Za-z0-9_ @,!$%-&*~.])*#\\d{4}",'i')
+  const regex = new RegExp("\\/(join|invite) ([A-Za-z0-9_ @,!$%\\-&*~.])*#\\d{4}",'i')
   
   return class DestinyLFGJoinInviteCopier extends Plugin {
     onStart() {
